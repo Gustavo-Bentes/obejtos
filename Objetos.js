d31c698cd01b35d2a10037f644e52153f16c1591
Gustavo-Bentes <gustavobentes05@gmail.com>
2022-08-15 02:32:01 -0300

    Create Objetos.js

---
+++ Objetos.js
@@ -0,0 +1,13 @@
+let carro = {
+    rodas: 5,
+    portas: 5,
+    nome: "celta",
+    Avenda: true
+};
+console.log(carro);
+
+carro.portas = 4;
+carro.nome = "fiesta";
+ 
+carro.portas = 4;
+console.log(carro)
